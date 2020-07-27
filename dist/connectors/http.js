729c02c48f4feda1c5cfe3bab0117c4139eaec18
Mona Lisa <razvan.pop@gmail.com>
2020-07-27 17:56:40 +0300

    implement a few methods

---
+++ dist/connectors/http.js
@@ -0,0 +1,108 @@
+"use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.HttpConnector = void 0;
+var node_fetch_1 = __importDefault(require("node-fetch"));
+var events_1 = require("events");
+var HttpConnector = (function (_super) {
+    __extends(HttpConnector, _super);
+    function HttpConnector(url) {
+        var _this = _super.call(this) || this;
+        _this.url = url;
+        _this.reqId = 0;
+        return _this;
+    }
+    HttpConnector.prototype.connect = function () {
+        return __awaiter(this, void 0, void 0, function () {
+            return __generator(this, function (_a) {
+                this.emit('connected');
+                return [2];
+            });
+        });
+    };
+    HttpConnector.prototype.disconnect = function () {
+        return __awaiter(this, void 0, void 0, function () {
+            return __generator(this, function (_a) {
+                this.emit('disconnected');
+                return [2];
+            });
+        });
+    };
+    HttpConnector.prototype.exec = function (method, params) {
+        return __awaiter(this, void 0, void 0, function () {
+            var message, _a, _b;
+            return __generator(this, function (_c) {
+                switch (_c.label) {
+                    case 0:
+                        message = {
+                            jsonrpc: "2.0",
+                            method: method,
+                            params: params || null,
+                            id: this.reqId,
+                        };
+                        _b = (_a = console).error;
+                        return [4, node_fetch_1.default(this.url, {
+                                method: 'POST',
+                                body: JSON.stringify(message),
+                            })];
+                    case 1:
+                        _b.apply(_a, [_c.sent()]);
+                        return [2];
+                }
+            });
+        });
+    };
+    return HttpConnector;
+}(events_1.EventEmitter));
+exports.HttpConnector = HttpConnector;
--- src/providers/Types.ts
+++ src/providers/Types.ts
@@ -1,22 +1,87 @@
 import { BigNumber } from 'bignumber.js';
 
-export interface Message {
-  Version: bigint;
-
-  To: string;
-
-  From: string;
-
-  Nonce: bigint;
-
-  Value: BigNumber;
-
-  GasPrice: BigNumber;
-
-  Method: bigint;
-
-  Params: ArrayBuffer;
+export class Cid {
+  '/'!: string;
 }
+
+export class BlockHeader {
+  Miner!: string;
+
+  Ticket!: {
+    VRFProof: string;
+  };
+
+  ElectionProof!: {
+    WinCount: number;
+    VRFProof: string;
+  };
+
+  BeaconEntries!: {
+    Round: number;
+    Data: string;
+  }[];
+
+  WinPoStProof!: {
+    PoStProof: number;
+    ProofBytes: string;
+  }[];
+
+  Parents!: Cid[];
+
+  ParentWeight!: string; // bn
+
+  Height!: number;
+
+  ParentStateRoot!: Cid;
+
+  ParentMessageReceipts!: Cid;
+
+  Messages!: Cid;
+
+  BLSAggregate!: {
+    Type: number;
+    Data: string;
+  };
+
+  Timestamp!: number;
+
+  BlockSig!: {
+    Type: number;
+    Data: string;
+  };
+
+  ForkSignaling!: number;
+}
+
+export class TipSet {
+  Cids!: Cid[];
+  Blocks!: BlockHeader[];
+  Height!: number;
+}
+
+export class Version {
+  Version!: string;
+  APIVersion!: number;
+  BlockDelay!: number;
+};
+
+export class Message {
+  Version!: bigint;
+
+  To!: string;
+
+  From!: string;
+
+  Nonce!: bigint;
+
+  Value!: BigNumber;
+
+  GasPrice!: BigNumber;
+
+  Method!: bigint;
+
+  Params!: ArrayBuffer;
+};
 
 export interface SignedMessage {
   Message: Message;
