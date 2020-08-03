1b87ab37eb8e8854d1b42f70e7c49bd0dd5ca0ef
Alexandru Matei <alexandrumatei3693@gmail.com>
2020-08-03 10:26:26 +0300

    Add webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -2,23 +2,17 @@ var path = require("path");
 var webpack = require("webpack");
 
 var PATHS = {
-  entryPoint: path.resolve(__dirname, 'src/index.ts'),
+  entryPoint: './src/index.ts',
   bundles: path.resolve(__dirname, '_bundles'),
 }
 
 var config = {
-  // These are the entry point of our library. We tell webpack to use
-  // the name we assign later, when creating the bundle. We also use
-  // the name to filter the second entry point for applying code
-  // minification via UglifyJS
+  mode: 'production',
+  devtool: 'source-map',
   entry: {
     'filecoin-js': [PATHS.entryPoint],
     'filecoin-js.min': [PATHS.entryPoint]
   },
-  // The output defines how and where we want the bundles. The special
-  // value `[name]` in `filename` tell Webpack to use the name we defined above.
-  // We target a UMD and name it MyLib. When including the bundle in the browser
-  // it will be accessible at `window.MyLib`
   output: {
     path: PATHS.bundles,
     filename: '[name].js',
@@ -26,18 +20,15 @@ var config = {
     library: 'FilecoinJs',
     umdNamedDefine: true
   },
-  // Add resolve for `tsx` and `ts` files, otherwise Webpack would
-  // only look for common JavaScript file extension (.js)
   resolve: {
     extensions: ['.ts', '.js']
   },
   // Activate source maps for the bundles in order to preserve the original
   // source when the user debugs the application
-  devtool: 'source-map',
   module: {
     rules: [
       {
-        test: /\.tsx?$/,
+        test: /\.ts?$/,
         loader: 'ts-loader',
         exclude: /(node_modules|__tests__)/,
       },
