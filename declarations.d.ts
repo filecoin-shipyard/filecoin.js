a117adf80e85ba508183d523d039c17b29801ab0
Razvan POP <razvan.pop@gmail.com>
2020-08-12 17:34:26 +0300

    webusb types

--- declarations.d.ts
+++ declarations.d.ts
@@ -1,5 +1,4 @@
 declare module '@zondax/filecoin-signing-tools/js';
-declare module '@ledgerhq/hw-transport-webusb';
 declare module '@zondax/filecoin-signing-tools';
 declare module "*import_wasm" {
     export const import_wasm: any;
