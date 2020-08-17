38e5a02cb902f890b8de220f4e448e23eeeb228b
Mona Lisa <razvan.pop@gmail.com>
2020-08-17 13:24:21 +0300

    cleanup examples

--- test-server/index.js
+++
@@ -1,8 +0,0 @@
-const static = require('node-static');
-const http = require('http');
-
-const file = new(static.Server)('../builds/bundle');
-file.serveFile()
-http.createServer(function (req, res) {
-  file.serve(req, res);
-}).listen(8080);
