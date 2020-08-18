bdc1da1bf8f3ecd5793b7265391fcdf3d69bc279
Alexandru Matei <alexandrumatei3693@gmail.com>
2020-08-18 12:22:11 +0300

    Add chain notify test

--- tests/src.ts/test-version.ts
+++ tests/src.ts/test-version.ts
@@ -1,34 +1,30 @@
 import assert from "assert";
 import { LOTUS_AUTH_TOKEN } from "../../testnet/credentials";
 import { JsonRpcProvider } from '../../src/providers/JsonRpcProvider';
-import { WsJsonRpcConnector } from '../../src/connectors/WsJsonRpcConnector';
 import { HttpJsonRpcConnector } from '../../src/connectors/HttpJsonRpcConnector';
 import { WebSocketProvider } from '../../src/providers/WebSocketProvider';
-const httpConnector = new HttpJsonRpcConnector({ url: 'http://localhost:8000/rpc/v0', token: LOTUS_AUTH_TOKEN });
-const wsConnector = new WsJsonRpcConnector({ url: 'http://localhost:8000/rpc/v0', token: LOTUS_AUTH_TOKEN });
+const httpConnector = new HttpJsonRpcConnector({ url: 'http://lotus-2a.testnet.s.interplanetary.one:1234/rpc/v0', token: LOTUS_AUTH_TOKEN });
 
 describe("Connection test", function () {
   it("check version", async function () {
     const con = new JsonRpcProvider(httpConnector);
     const version = await con.version();
-    assert.equal(version.APIVersion, 1536, 'wrong api version');
+    assert.equal(version.APIVersion, 2816, 'wrong api version');
   });
 
-  it("check version ws", async function () {
-    await wsConnector.connect();
-    const con = new JsonRpcProvider(wsConnector);
-    const version = await con.version();
-    assert.equal(version.APIVersion, 1536, 'wrong api version');
-    await wsConnector.disconnect();
+  it("should get version using ws", async function () {
+    const provider = new WebSocketProvider('ws://lotus-2a.testnet.s.interplanetary.one:1234/rpc/v0');
+    const version = await provider.version();
+    assert.strictEqual(version.APIVersion, 2816, 'wrong api version');
+    await provider.release();
   });
 
-  it("check chain notify", function(done) {
-    const provider = new WebSocketProvider('http://localhost:8000/rpc/v0');
-    provider.connector.connect().then(() => {
-      provider.chainNotify((headChange) => {
-        assert.strictEqual(headChange[0].Type, "current", "wrong chain head type");
-        done();
-      });
-    });
+  it("should be notified on chain head change", function(done) {
+    const provider = new WebSocketProvider('ws://lotus-2a.testnet.s.interplanetary.one:1234/rpc/v0');
+    provider.chainNotify(headChange => {
+      const type = headChange[0].Type;
+      assert.strictEqual(type, "current", "wrong chain head type");
+      provider.release().then(() => { done() });
+    })
   });
 });
