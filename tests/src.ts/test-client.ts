5e93f1d0d2fdf7893d615d38687ad49aa68fcb87
Alexandru Matei <alexandrumatei3693@gmail.com>
2020-09-10 11:28:45 +0300

    Separate client tests

---
+++ tests/src.ts/test-client.ts
@@ -0,0 +1,205 @@
+import { HttpJsonRpcWalletProvider, JsonRpcProvider } from '../../src';
+import assert from "assert";
+import { HttpJsonRpcConnector } from '../../src/connectors/HttpJsonRpcConnector';
+import { LOTUS_AUTH_TOKEN } from '../../testnet/credentials/credentials';
+import { WsJsonRpcConnector } from '../../src/connectors/WsJsonRpcConnector';
+
+const httpConnector = new HttpJsonRpcConnector({ url: 'http://localhost:8000/rpc/v0', token: LOTUS_AUTH_TOKEN });
+const walletLotus = new HttpJsonRpcWalletProvider({ url: 'http://localhost:8000/rpc/v0', token: LOTUS_AUTH_TOKEN });
+
+describe.only("Client tests", function() {
+  it("should import file", async function() {
+    const provider = new JsonRpcProvider(httpConnector);
+    const result = await provider.import({
+      Path: "/filecoin_miner/original-data.txt",
+      IsCAR: false,
+    });
+    const valid = typeof result.Root["/"] === 'string' && typeof result.ImportID === 'number';
+    assert.strictEqual(valid, true, 'import file failed');
+  });
+
+  it("should delete imported file", async function() {
+    const provider = new JsonRpcProvider(httpConnector);
+    const importResult = await provider.import({
+      Path: "/filecoin_miner/original-data.txt",
+      IsCAR: false,
+    });
+    await provider.removeImport(importResult.ImportID);
+  });
+
+  it("should start deal", async function() {
+    const provider = new JsonRpcProvider(httpConnector);
+    const importResult = await provider.import({
+      Path: "/filecoin_miner/original-data.txt",
+      IsCAR: false,
+    });
+    const dealCid = await provider.startDeal({
+      Data: {
+        TransferType: 'graphsync',
+        Root: importResult.Root,
+      },
+      Miner: 't01000',
+      Wallet: await walletLotus.getDefaultAccount(),
+      EpochPrice: '1001',
+      MinBlocksDuration: 800,
+    });
+    assert.strictEqual(typeof dealCid["/"] === 'string', true, 'deal start failed');
+  });
+
+  it("should get deal info", async function() {
+    const provider = new JsonRpcProvider(httpConnector);
+    const importResult = await provider.import({
+      Path: "/filecoin_miner/original-data.txt",
+      IsCAR: false,
+    });
+    const dealCid = await provider.startDeal({
+      Data: {
+        TransferType: 'graphsync',
+        Root: importResult.Root,
+      },
+      Miner: 't01000',
+      Wallet: await walletLotus.getDefaultAccount(),
+      EpochPrice: '1002',
+      MinBlocksDuration: 800,
+    });
+    const dealInfo = await provider.getDealInfo(dealCid);
+    const valid = !!dealInfo.Provider && !!dealInfo.State;
+    assert.strictEqual(valid, true, 'invalid deal info');
+  });
+
+  it("should list all deals", async function() {
+    const provider = new JsonRpcProvider(httpConnector);
+    const deals = await provider.listDeals();
+    assert.strictEqual(Array.isArray(deals), true, 'invalid deals list');
+  });
+
+  it("should verify if has local", async function() {
+    const provider = new JsonRpcProvider(httpConnector);
+    const importResult = await provider.import({
+      Path: "/filecoin_miner/original-data.txt",
+      IsCAR: false,
+    });
+    const hasLocal = await provider.hasLocal(importResult.Root);
+    assert.strictEqual(hasLocal, true, 'invalid has local');
+  });
+
+  it("should find data", async function() {
+    const provider = new JsonRpcProvider(httpConnector);
+    const importResult = await provider.import({
+      Path: "/filecoin_miner/original-data.txt",
+      IsCAR: false,
+    });
+    await provider.startDeal({
+      Data: {
+        TransferType: 'graphsync',
+        Root: importResult.Root,
+      },
+      Miner: 't01000',
+      Wallet: await walletLotus.getDefaultAccount(),
+      EpochPrice: '1003',
+      MinBlocksDuration: 800,
+    });
+    const queryOffers = await provider.findData(importResult.Root);
+    const isValid = queryOffers.reduce((acc, offer, idx) => acc === false ? acc : offer.Root["/"] === importResult.Root["/"], true);
+    assert.strictEqual(isValid, true, 'invalid found data');
+  });
+
+  it("should get miner query offer", async function() {
+    const provider = new JsonRpcProvider(httpConnector);
+    const importResult = await provider.import({
+      Path: "/filecoin_miner/original-data.txt",
+      IsCAR: false,
+    });
+    const queryOffer = await provider.minerQueryOffer('t01000', importResult.Root);
+    const isValid = importResult.Root["/"] === queryOffer.Root["/"];
+    assert.strictEqual(isValid, true, 'invalid miner query offer');
+  });
+
+  // Sealing takes too much
+  // it.only("should retrieve file", async function() {
+  //   const provider = new JsonRpcProvider(httpConnector);
+  //   const importResult = await provider.import({
+  //     Path: "/filecoin_miner/original-data.txt",
+  //     IsCAR: false,
+  //   });
+  //   const dealCid = await provider.startDeal({
+  //     Data: {
+  //       TransferType: 'graphsync',
+  //       Root: importResult.Root,
+  //     },
+  //     Miner: 't01000',
+  //     Wallet: await walletLotus.getDefaultAccount(),
+  //     EpochPrice: '1000',
+  //     MinBlocksDuration: 700000,
+  //   });
+  //   const queryOffer = await provider.minerQueryOffer('t01000', importResult.Root);
+  //   await provider.retrieve({
+  //     Root: queryOffer.Root,
+  //     Size: queryOffer.Size,
+  //     Total: queryOffer.MinPrice,
+  //     UnsealPrice: "0",
+  //     PaymentInterval: queryOffer.PaymentInterval,
+  //     PaymentIntervalIncrease: queryOffer.PaymentIntervalIncrease,
+  //     Client: queryOffer.Miner,
+  //     Miner: queryOffer.Miner,
+  //     MinerPeer: {
+  //       Address: queryOffer.MinerPeer.Address,
+  //       ID: queryOffer.MinerPeer.ID
+  //     },
+  //   }, {
+  //     Path: "/filecoin_miner/original-data-retrieved.txt",
+  //     IsCAR: false,
+  //   });
+  // });
+
+  it("should perform query ask ", async function() {
+    const provider = new JsonRpcProvider(httpConnector);
+    const minerInfo = await provider.minerInfo('t01000');
+    const queryOffer = await provider.queryAsk(minerInfo.PeerId, 't01000');
+    assert.strictEqual(!!queryOffer.Ask && !!queryOffer.Signature, true, 'failed query ask');
+  });
+
+  it("should compute commP", async function() {
+    const provider = new JsonRpcProvider(httpConnector);
+    const { Root, Size } = await provider.calcCommP("/filecoin_miner/original-data.txt");
+    assert.strictEqual(!!Root && !!Size, true, 'failed to compute commP');
+  });
+
+  it("should generate CAR file", async function() {
+    const provider = new JsonRpcProvider(httpConnector);
+    const car = await provider.genCar({
+      IsCAR: false,
+      Path: "/filecoin_miner/original-data.txt",
+    }, "/filecoin_miner/car.txt");
+  });
+
+  it("should calculate deal size", async function() {
+    const provider = new JsonRpcProvider(httpConnector);
+    const importResult = await provider.import({
+      Path: "/filecoin_miner/original-data.txt",
+      IsCAR: false,
+    });
+    const { PayloadSize, PieceSize } = await provider.dealSize(importResult.Root);
+    const isValid = typeof PayloadSize === 'number' && typeof PieceSize === 'number';
+    assert.strictEqual(isValid, true, 'invalid deal size');
+  });
+
+  it("should get transfers status", async function() {
+    const provider = new JsonRpcProvider(httpConnector);
+    const transfers = await provider.listDataTransfers();
+    assert.strictEqual(Array.isArray(transfers), true, 'invalid transfers status');
+  });
+
+  it("should list imports", async function() {
+    const provider = new JsonRpcProvider(httpConnector);
+    const importResult = await provider.import({
+      Path: "/filecoin_miner/original-data.txt",
+      IsCAR: false,
+    });
+    const imports = await provider.listImports();
+    const isValid = imports.filter(importItem => (
+      importItem.Root && importItem.Root['/'] === importResult.Root['/'] && importItem.Key === importResult.ImportID
+    )).length > 0;
+    assert.strictEqual(isValid, true, 'invalid imports list');
+  });
+});
