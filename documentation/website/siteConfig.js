52e0e2d9fc6a5f8157c656ae320f875e3309605c
Alexandru Matei <alexandrumatei3693@gmail.com>
2020-09-22 14:30:19 +0300

    Update footer with GitHub repo infos

--- documentation/website/siteConfig.js
+++ documentation/website/siteConfig.js
@@ -108,7 +108,7 @@ const siteConfig = {
 
   // You may provide arbitrary config keys to be used as needed by your
   // template. For example, if you need your repo's URL...
-    repoUrl: 'https://github.com',
+    repoUrl: 'https://github.com/Digital-MOB-Filecoin/filecoin.js/',
 };
 
 module.exports = siteConfig;
