34b80007e8214010b488a12e88883ee5895b1137
bc <bara.catalin@gmail.com>
2020-09-30 13:31:01 +0300

    run all tests in browser

--- tests/src/index.ts
+++ tests/src/index.ts
@@ -1,5 +1,16 @@
 "use strict";
 
+import "./test-auth";
+import "./test-chain";
+import "./test-client";
+import "./test-common";
+import "./test-gas";
+import "./test-miner";
+import "./test-mpool";
+import "./test-multisig";
+import "./test-net";
+import "./test-paych";
+import "./test-wallet";
 import "./test-version";
 import "./test-send-message";
 
