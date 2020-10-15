71353742655272c4628593ac0e5ebe1ceadae59b
Alexandru Matei <alexandrumatei3693@gmail.com>
2020-10-15 11:09:29 +0300

    Refresh documentation website

--- documentation/website/core/Footer.js
+++ documentation/website/core/Footer.js
@@ -25,17 +25,17 @@ class Footer extends React.Component {
     return (
       <footer className="nav-footer" id="footer">
         <section className="sitemap">
-          <a href={this.props.config.baseUrl} className="nav-home">
-            {this.props.config.footerIcon && (
-              <img
-                src={this.props.config.baseUrl + this.props.config.footerIcon}
-                alt={this.props.config.title}
-                style={{
-                  width: '100%'
-                }}
-              />
-            )}
-          </a>
+          {/*<a href={this.props.config.baseUrl} className="nav-home">*/}
+          {/*  {this.props.config.footerIcon && (*/}
+          {/*    <img*/}
+          {/*      src={this.props.config.baseUrl + this.props.config.footerIcon}*/}
+          {/*      alt={this.props.config.title}*/}
+          {/*      style={{*/}
+          {/*        width: '100%'*/}
+          {/*      }}*/}
+          {/*    />*/}
+          {/*  )}*/}
+          {/*</a>*/}
           <div>
             <h5>Docs</h5>
             <a href={this.docUrl('doc1.html', this.props.language)}>
