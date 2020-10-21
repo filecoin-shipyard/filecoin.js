be156fffa6370a9b834d8931d0d6d300a0604f44
Alexandru Matei <alexandrumatei3693@gmail.com>
2020-10-21 16:50:55 +0300

    Update release config to skip npm publish

--- release.config.js
+++ release.config.js
@@ -9,7 +9,8 @@ module.exports = {
     // updates the version in package.json and creates a tarball in the release directory based on the files specified in package.json.
     // If the package isn’t marked as private in package.json, the new version of the package is published to NPM.
     ["@semantic-release/npm", {
-      "tarballDir": "release"
+      "tarballDir": "release",
+      "npmPublish": false
     }],
     // creates a GitHub release titled and tagged with the new version.
     // The release notes are used in the description and the tarball created in the previous step is included as the release binary.
